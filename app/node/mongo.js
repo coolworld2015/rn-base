9dd8b14aef75d3ddc3e68082c53ecc535af1dd31
coolworld2015 <wintermute2015@ukr.net>
2019-08-25 14:15:30 +0300

    [general] - refactoring

--- app/node/mongo.js
+++ app/node/mongo.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 
-var options = { server: { socketOptions: { connectTimeoutMS: 60000 }}};
+var options = {server: {socketOptions: {connectTimeoutMS: 60000}}};
 
 mongoose.connect('mongodb://admin:admin@ds011258.mongolab.com:11258/ui-base', options);
 //mongoose.connect('mongodb://localhost:27017/ui-base'); //TODO ONLY for Local INSTANCE MongoDB
 
 var db = mongoose.connection;
 
-db.on('error', function (err) {
+db.on('error', function(err) {
     console.log('Error from mongoDB: ' + err.message);
 });
 
@@ -21,7 +21,7 @@ var Schema = mongoose.Schema;
 var Items = new Schema({
     id: {type: String, required: true},
     name: {type: String, required: true},
-    phone: {type: String, required: true}
+    phone: {type: String, required: true},
 });
 
 var ItemsModel = mongoose.model('Items', Items);
@@ -32,7 +32,7 @@ var Users = new Schema({
     id: {type: String, required: true},
     name: {type: String, required: true},
     pass: {type: String, required: true},
-    description: {type: String, required: true}
+    description: {type: String, required: true},
 });
 
 var UsersModel = mongoose.model('Users', Users);
@@ -44,7 +44,7 @@ var Audit = new Schema({
     name: {type: String, required: true},
     date: {type: String, required: true},
     ip: {type: String, required: true},
-    description: {type: String, required: true}
+    description: {type: String, required: true},
 });
 
 var AuditModel = mongoose.model('Audit', Audit);
--- app/src/android/app/appContainer.js
+++ app/src/android/app/appContainer.js
@@ -2,11 +2,11 @@
 
 import React, {Component} from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  DrawerLayoutAndroid
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    DrawerLayoutAndroid,
 } from 'react-native';
 
 import NavigationExperimental from 'react-native-deprecated-custom-components';
@@ -26,284 +26,284 @@ import Audit from '../audit/audit';
 import AuditDetails from '../audit/auditDetails';
 
 class AppContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  onBack() {
-    appConfig.drawer.closeDrawer();
-  }
+    onBack() {
+        appConfig.drawer.closeDrawer();
+    }
 
-  componentDidMount() {
-    appConfig.drawer = this.refs['DRAWER_REF'];
-  }
+    componentDidMount() {
+        appConfig.drawer = this.refs['DRAWER_REF'];
+    }
 
-  onLogOut() {
-    appConfig.onLogOut();
-  }
+    onLogOut() {
+        appConfig.onLogOut();
+    }
 
-  render() {
-    let navigationView = (
-      <View style={{flex: 1, backgroundColor: '#fff'}}>
-        <Text style={{margin: 10, fontSize: 25, textAlign: 'center', color: 'black'}}>
-          Menu
-        </Text>
+    render() {
+        let navigationView = (
+            <View style={{flex: 1, backgroundColor: '#fff'}}>
+                <Text style={{margin: 10, fontSize: 25, textAlign: 'center', color: 'black'}}>
+                    Menu
+                </Text>
 
-        <TouchableHighlight
-          onPress={() => this.onLogOut()}
-          style={styles.button}>
-          <Text style={styles.buttonText}>
-            Log out
-          </Text>
-        </TouchableHighlight>
+                <TouchableHighlight
+                    onPress={() => this.onLogOut()}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Log out
+                    </Text>
+                </TouchableHighlight>
 
-        <TouchableHighlight
-          onPress={() => this.onBack()}
-          style={styles.button}>
-          <Text style={styles.buttonText}>
-            Back
-          </Text>
-        </TouchableHighlight>
-      </View>
-    );
+                <TouchableHighlight
+                    onPress={() => this.onBack()}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Back
+                    </Text>
+                </TouchableHighlight>
+            </View>
+        );
 
-    return (
-      <DrawerLayoutAndroid
-        ref={'DRAWER_REF'}
-        drawerWidth={200}
-        drawerPosition={DrawerLayoutAndroid.positions.Left}
-        renderNavigationView={() => navigationView}>
-        <ScrollableTabView
-          renderTabBar={() => <DefaultTabBar
-            activeTextColor='darkblue'
-            inactiveTextColor='darkblue'
-            underlineStyle={{backgroundColor: 'darkblue'}}
-            backgroundColor='white'/>}>
+        return (
+            <DrawerLayoutAndroid
+                ref={'DRAWER_REF'}
+                drawerWidth={200}
+                drawerPosition={DrawerLayoutAndroid.positions.Left}
+                renderNavigationView={() => navigationView}>
+                <ScrollableTabView
+                    renderTabBar={() => <DefaultTabBar
+                        activeTextColor='darkblue'
+                        inactiveTextColor='darkblue'
+                        underlineStyle={{backgroundColor: 'darkblue'}}
+                        backgroundColor='white'/>}>
 
-          <PhonesTab tabLabel="Phones"/>
-          <UsersTab tabLabel="Users"/>
-          <AuditTab tabLabel="Audit"/>
-          <Logout tabLabel="Quit"/>
+                    <PhonesTab tabLabel="Phones"/>
+                    <UsersTab tabLabel="Users"/>
+                    <AuditTab tabLabel="Audit"/>
+                    <Logout tabLabel="Quit"/>
 
-        </ScrollableTabView>
-      </DrawerLayoutAndroid>
-    )
-  }
+                </ScrollableTabView>
+            </DrawerLayoutAndroid>
+        );
+    }
 }
 
 class PhonesTab extends Component {
-  constructor(props) {
-    super(props);
-    this.routes = [
-      {title: 'Phones', index: 0},
-      {title: 'Phones Details', index: 1}
-    ];
-  }
-
-  renderScene(route, navigator) {
-    switch (route.index) {
-      case 0:
-        return <Phones routes={this.routes} navigator={navigator}/>;
-        break;
-      case 1:
-        return <PhoneDetails data={route.data} routes={this.routes} navigator={navigator}/>;
-        break;
-      case 2:
-        return <Search data={route.data} routes={this.routes} navigator={navigator}/>;
-        break;
-      case 3:
-        return <SearchResults data={route.data} routes={this.routes} navigator={navigator}/>;
-        break;
+    constructor(props) {
+        super(props);
+        this.routes = [
+            {title: 'Phones', index: 0},
+            {title: 'Phones Details', index: 1},
+        ];
     }
-  }
 
-  render() {
-    return (
-      <NavigationExperimental.Navigator
-        initialRoute={this.routes[0]}
-        initialRouteStack={this.routes}
-        renderScene={this.renderScene.bind(this)}
-        configureScene={(route, routeStack) =>
-          NavigationExperimental.Navigator.SceneConfigs.PushFromRight}
-      />
-    )
-  }
+    renderScene(route, navigator) {
+        switch (route.index) {
+            case 0:
+                return <Phones routes={this.routes} navigator={navigator}/>;
+                break;
+            case 1:
+                return <PhoneDetails data={route.data} routes={this.routes} navigator={navigator}/>;
+                break;
+            case 2:
+                return <Search data={route.data} routes={this.routes} navigator={navigator}/>;
+                break;
+            case 3:
+                return <SearchResults data={route.data} routes={this.routes} navigator={navigator}/>;
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <NavigationExperimental.Navigator
+                initialRoute={this.routes[0]}
+                initialRouteStack={this.routes}
+                renderScene={this.renderScene.bind(this)}
+                configureScene={(route, routeStack) =>
+                    NavigationExperimental.Navigator.SceneConfigs.PushFromRight}
+            />
+        );
+    }
 }
 
 class UsersTab extends Component {
-  constructor(props) {
-    super(props);
-    this.routes = [
-      {title: 'Users', index: 0},
-      {title: 'Users Details', index: 1},
-      {title: 'Add User', index: 2},
-    ];
-  }
-
-  renderScene(route, navigator) {
-    switch (route.index) {
-      case 0:
-        return <Users routes={this.routes} navigator={navigator}/>;
-        break;
-      case 1:
-        return <UserDetails data={route.data} routes={this.routes} navigator={navigator}/>;
-        break;
-      case 2:
-        return <UserAdd data={route.data} routes={this.routes} navigator={navigator}/>;
-        break;
+    constructor(props) {
+        super(props);
+        this.routes = [
+            {title: 'Users', index: 0},
+            {title: 'Users Details', index: 1},
+            {title: 'Add User', index: 2},
+        ];
     }
-  }
 
-  render() {
-    return (
-      <NavigationExperimental.Navigator
-        initialRoute={this.routes[0]}
-        initialRouteStack={this.routes}
-        renderScene={this.renderScene.bind(this)}
-        configureScene={(route, routeStack) =>
-          NavigationExperimental.Navigator.SceneConfigs.PushFromRight}
-      />
-    );
-  }
+    renderScene(route, navigator) {
+        switch (route.index) {
+            case 0:
+                return <Users routes={this.routes} navigator={navigator}/>;
+                break;
+            case 1:
+                return <UserDetails data={route.data} routes={this.routes} navigator={navigator}/>;
+                break;
+            case 2:
+                return <UserAdd data={route.data} routes={this.routes} navigator={navigator}/>;
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <NavigationExperimental.Navigator
+                initialRoute={this.routes[0]}
+                initialRouteStack={this.routes}
+                renderScene={this.renderScene.bind(this)}
+                configureScene={(route, routeStack) =>
+                    NavigationExperimental.Navigator.SceneConfigs.PushFromRight}
+            />
+        );
+    }
 }
 
 class AuditTab extends Component {
-  constructor(props) {
-    super(props);
-    this.routes = [
-      {title: 'Audit', index: 0},
-      {title: 'Audit Details', index: 1},
-      {title: 'Add Audit', index: 2},
-    ];
-  }
-
-  renderScene(route, navigator) {
-    switch (route.index) {
-      case 0:
-        return <Audit routes={this.routes} navigator={navigator}/>;
-        break;
-      case 1:
-        return <AuditDetails data={route.data} routes={this.routes} navigator={navigator}/>;
-        break;
+    constructor(props) {
+        super(props);
+        this.routes = [
+            {title: 'Audit', index: 0},
+            {title: 'Audit Details', index: 1},
+            {title: 'Add Audit', index: 2},
+        ];
     }
-  }
 
-  render() {
-    return (
-      <NavigationExperimental.Navigator
-        initialRoute={this.routes[0]}
-        initialRouteStack={this.routes}
-        renderScene={this.renderScene.bind(this)}
-        configureScene={(route, routeStack) =>
-          NavigationExperimental.Navigator.SceneConfigs.PushFromRight}
-      />
-    );
-  }
+    renderScene(route, navigator) {
+        switch (route.index) {
+            case 0:
+                return <Audit routes={this.routes} navigator={navigator}/>;
+                break;
+            case 1:
+                return <AuditDetails data={route.data} routes={this.routes} navigator={navigator}/>;
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <NavigationExperimental.Navigator
+                initialRoute={this.routes[0]}
+                initialRouteStack={this.routes}
+                renderScene={this.renderScene.bind(this)}
+                configureScene={(route, routeStack) =>
+                    NavigationExperimental.Navigator.SceneConfigs.PushFromRight}
+            />
+        );
+    }
 }
 
 class Logout extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    appConfig.onLogOut();
-  }
+        appConfig.onLogOut();
+    }
 
-  render() {
-    return null;
-  }
+    render() {
+        return null;
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  iconForm: {
-    flexDirection: 'row',
-    borderColor: 'darkblue',
-    borderWidth: 3,
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'darkblue',
-    borderWidth: 0,
-    borderColor: 'whitesmoke',
-  },
-  textSmall: {
-    fontSize: 16,
-    textAlign: 'center',
-    margin: 14,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  textLarge: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    marginTop: 12,
-    paddingLeft: 20,
-    marginLeft: -10,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  textInput: {
-    height: 45,
-    marginTop: 0,
-    padding: 5,
-    backgroundColor: 'white',
-    borderWidth: 3,
-    borderColor: 'lightgray',
-    borderRadius: 0,
-  },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 20,
-    alignItems: 'center',
-    borderColor: '#D7D7D7',
-    borderBottomWidth: 1,
-    backgroundColor: '#fff',
-  },
-  rowText: {
-    backgroundColor: '#fff',
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  countFooter: {
-    fontSize: 16,
-    textAlign: 'center',
-    padding: 10,
-    borderColor: '#D7D7D7',
-    backgroundColor: 'darkblue',
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  loader: {
-    justifyContent: 'center',
-    height: 100,
-  },
-  error: {
-    color: 'red',
-    paddingTop: 10,
-    textAlign: 'center',
-  },
-  button: {
-    height: 50,
-    backgroundColor: 'darkblue',
-    borderColor: '#48BBEC',
-    alignSelf: 'stretch',
-    marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'white',
+    },
+    iconForm: {
+        flexDirection: 'row',
+        borderColor: 'darkblue',
+        borderWidth: 3,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'darkblue',
+        borderWidth: 0,
+        borderColor: 'whitesmoke',
+    },
+    textSmall: {
+        fontSize: 16,
+        textAlign: 'center',
+        margin: 14,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    textLarge: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+        marginTop: 12,
+        paddingLeft: 20,
+        marginLeft: -10,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    textInput: {
+        height: 45,
+        marginTop: 0,
+        padding: 5,
+        backgroundColor: 'white',
+        borderWidth: 3,
+        borderColor: 'lightgray',
+        borderRadius: 0,
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 20,
+        alignItems: 'center',
+        borderColor: '#D7D7D7',
+        borderBottomWidth: 1,
+        backgroundColor: '#fff',
+    },
+    rowText: {
+        backgroundColor: '#fff',
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    countFooter: {
+        fontSize: 16,
+        textAlign: 'center',
+        padding: 10,
+        borderColor: '#D7D7D7',
+        backgroundColor: 'darkblue',
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    loader: {
+        justifyContent: 'center',
+        height: 100,
+    },
+    error: {
+        color: 'red',
+        paddingTop: 10,
+        textAlign: 'center',
+    },
+    button: {
+        height: 50,
+        backgroundColor: 'darkblue',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
 });
 
 export default AppContainer;
